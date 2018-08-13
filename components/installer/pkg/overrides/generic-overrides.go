946b232e22cbe978f50a93a26e610968bce94a51
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-08-13 12:36:27 +0200

    Generic overrides: Merge map to yaml

---
+++ components/installer/pkg/overrides/generic-overrides.go
@@ -0,0 +1,38 @@
+package overrides
+
+import (
+	"strings"
+
+	"github.com/ghodss/yaml"
+)
+
+//Merges value into given map, introducing intermediate "nested" maps for every intermediate key.
+func mergeIntoMap(keys []string, value string, dstMap map[string]interface{}) {
+	currentKey := keys[0]
+	//Last key points directly to string value
+	if len(keys) == 1 {
+		dstMap[currentKey] = value
+		return
+	}
+
+	//All keys but the last one should point to a nested map
+	nestedMap, ok := dstMap[currentKey].(map[string]interface{})
+
+	if !ok {
+		nestedMap = map[string]interface{}{}
+		dstMap[currentKey] = nestedMap
+	}
+
+	mergeIntoMap(keys[1:], value, nestedMap)
+}
+
+func mapToYaml(sourceMap map[string]string) string {
+	mergedMap := map[string]interface{}{}
+
+	for key, value := range sourceMap {
+		keys := strings.Split(key, ".")
+		mergeIntoMap(keys, value, mergedMap)
+	}
+
+	return yaml.Marshal(mergedMap)
+}
