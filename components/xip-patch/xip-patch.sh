15b1226c56c53ce4300fcf1c28a1df1ff6adc501
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2019-10-30 16:15:48 +0100

    Roles

--- components/xip-patch/xip-patch.sh
+++ components/xip-patch/xip-patch.sh
@@ -42,20 +42,23 @@ generateXipDomain() {
 
 generateCerts() {
 
+    rm -f /tmp/ca.key
+    rm -f /tmp/ca.crt
+
     # Generate a Root CA private key
-    openssl genrsa -out ca.key 2048
+    openssl genrsa -out /tmp/ca.key 2048
 
     # Create a Root CA: self signed Certificate, valid for 10yrs with the 'signing' option set
-    openssl req -x509 -new -nodes -key ca.key -subj "/CN=kymulec" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+    openssl req -x509 -new -nodes -key /tmp/ca.key -subj "/CN=kymulec" -days 3650 -reqexts v3_req -extensions v3_ca -out /tmp/ca.crt
 
     # Store Root CA key pair as secret (necessary for cert-manager to issue certificates based on the Root CA)
     kubectl create secret tls kyma-ca-key-pair \
-      --cert=ca.crt \
-      --key=ca.key \
+      --cert=/tmp/ca.crt \
+      --key=/tmp/ca.key \
       --namespace=istio-system
 
     # export Root CA public key so internal and external clients can understand certs issued by cert-manager and signed by the Root CA
-    export INGRESS_TLS_CERT=$(base64 < ca.crt)
+    export INGRESS_TLS_CERT=$(base64 < /tmp/ca.crt)
 
     TEMP=$(mktemp /tmp/cert-file.XXXXXXXX)
     sed 's/{{.Values.global.ingress.domainName}}/'$INGRESS_DOMAIN'/' /etc/cert-config/config.yaml.tpl > ${TEMP}
